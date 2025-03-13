7a71df1f6b300bc56731c71602b874e769eff65a
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-13 21:35:48 +0000

    Installing tmux

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/ap
 echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo apt install wezterm
 
+echo "Installing tmux..."
+apt install tmux
+
 echo "Cloning Zaidan..."
 rm -rf ~/.local/share/zaidan
 git clone https://github.com/LiamWalshTech/Zaidan.git ~/.local/share/zaidan >/dev/null
