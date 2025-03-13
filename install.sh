b92ffc2da998c6260a32ef386e2c650b1985f905
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-13 22:30:11 +0000

    Fixed go

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ source ~/.local/share/zaidan/install/chrome.sh
 source ~/.local/share/zaidan/install/docker.sh
 # source ~/.local/share/zaidan/install/fonts.sh
 source ~/.local/share/zaidan/install/gh.sh
-source ~/.local/share/zaidan/instal/go.sh
+source ~/.local/share/zaidan/install/go.sh
 source ~/.local/share/zaidan/install/lazydocker.sh
 source ~/.local/share/zaidan/install/lazygit.sh
 source ~/.local/share/zaidan/install/neovim.sh
