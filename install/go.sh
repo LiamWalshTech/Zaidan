df32fa926ba3c2078c0f9541b8b047a4a08ffd93
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-13 22:37:41 +0000

    Pull down tar

--- install/go.sh
+++ install/go.sh
@@ -1,3 +1,3 @@
- rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.1.linux-amd64.tar.gz
+ rm -rf /usr/local/go && tar -C /usr/local -xzf https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
  export PATH=$PATH:/usr/local/go/bin
-  go version
+ go version
