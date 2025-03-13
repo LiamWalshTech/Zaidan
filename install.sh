c3210fc48f3deb454b654a0e3d21c1b7e1f2c908
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-13 20:06:23 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+# Libraries and infrastructure
+sudo apt update -y
+
+# CLI apps
+sudo apt install -y git curl btop
+
+# GUI apps
+sudo snap install 1password vlc
+
+# # Installers
+# source install/chrome.sh
+# source install/fonts.sh
+# source install/ulauncher.sh
+# source install/neovim.sh
+# source install/lazygit.sh
+# source install/lazydocker.sh
+# source install/gh.sh
+# source install/docker.sh
+
+# Configuration
+
+# # Start
+# gh auth login
