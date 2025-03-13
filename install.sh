dddceccbe294e78c7b69800afb492c7ff47af7aa
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-13 21:33:35 +0000

    Switching to Wezterm

--- install.sh
+++ install.sh
@@ -4,15 +4,19 @@
 echo "Libraries and infrastructure..."
 sudo apt update -y
 
-echo "Insatlling CLI Apps..."
+echo "Insatlling cli apps..."
 sudo apt install -y git curl fzf ripgrep fd btop
-sudo apt install -y alacritty
+
+echo "Installing better terminal"
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo apt install wezterm
 
 echo "Cloning Zaidan..."
 rm -rf ~/.local/share/zaidan
 git clone https://github.com/LiamWalshTech/Zaidan.git ~/.local/share/zaidan >/dev/null
 
-echo "GUI Apps..."
+echo "GUI apps..."
 sudo snap install 1password vlc
 
 echo "Installers"
