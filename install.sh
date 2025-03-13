70d53073aed08b30b3356fbb452f2d05482c63f9
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-13 20:39:14 +0000

    Now clones the repo

--- install.sh
+++ install.sh
@@ -1,23 +1,30 @@
 # Libraries and infrastructure
-sudo apt update -y
+sudo apt update -y >/dev/null
 
 # CLI apps
-sudo apt install -y git curl btop
+sudo apt install -y git curl btop >/dev/null
 
-# GUI apps
+echo "Cloning Zaidan..."
+rm -rf ~/.local/share/zaidan
+git clone https://github.com/LiamWalshTech/Zaidan.git ~/.local/share/zaidan >/dev/null
+
+echo "Installation starting..."
+source ~/.local/share/zaidan/install.sh
+
+echo "GUI Apps..."
 sudo snap install 1password vlc
 
-# # Installers
-# source install/chrome.sh
-# source install/fonts.sh
-# source install/ulauncher.sh
-source install/neovim.sh
-# source install/lazygit.sh
-# source install/lazydocker.sh
-# source install/gh.sh
-# source install/docker.sh
+echo "Installers"
+# source ~/.local/share/zaidan/install/chrome.sh
+# source ~/.local/share/zaidan/install/fonts.sh
+# source ~/.local/share/zaidan/install/ulauncher.sh
+source ~/.local/share/zaidan/install/neovim.sh
+# source ~/.local/share/zaidan/install/lazygit.sh
+# source ~/.local/share/zaidan/install/lazydocker.sh
+# source ~/.local/share/zaidan/install/gh.sh
+# source ~/.local/share/zaidan/install/docker.sh
 
-# Configuration
+echo "Configuration"
 
 # # Start
 # gh auth login
