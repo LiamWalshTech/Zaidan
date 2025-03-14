3853e8ae4b36e5ded37f4ad153ee8494be1235b2
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-14 23:46:05 +0000

    Switch to i3

--- install.sh
+++ install.sh
@@ -40,3 +40,11 @@ echo "Configuration"
 
 echo "Cleanup..."
 sudo apt autoremove -y
+
+if pgrep -x "i3" > /dev/null
+then
+  echo "Congratulations you're in i3 and done"
+else
+  echo "You'll need to logout to be able to switch to i3"
+  gnome-session-quit --logout
+fi
