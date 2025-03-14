b49c22269fa753e7ba8b047b240a54111afff703
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-14 22:43:59 +0000

    Fixing sudo problem

--- install/go.sh
+++ install/go.sh
@@ -1,5 +1,5 @@
 wget https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
-rm -rf /usr/local/go
+sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf ./go1.24.1.linux-amd64.tar.gz
 rm go1.24.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
