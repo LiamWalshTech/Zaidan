a4837832b41b6472a3e878e06e024c528b8dee99
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-15 00:09:32 +0000

    Move the polybar launch file

--- install/i3wm.sh
+++ install/i3wm.sh
@@ -1,3 +1,4 @@
 sudo apt install -y i3
 sudo mv ~/.local/share/zaidan/configs/40-libinput.conf /usr/share/X11/xorg.conf.d/40-libinput.conf
 sudo apt install polybar
+sudo mv ~/.local/share/zaidan/configs/polybar-launch.sh ~/.config/i3/launch_polybar.sh
