cf7226447d4cd3202f72a3775b9c965d70ba04e8
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-15 00:30:35 +0000

    Make sure it's executable

--- install/i3wm.sh
+++ install/i3wm.sh
@@ -2,5 +2,6 @@ sudo apt install -y i3
 sudo mv ~/.local/share/zaidan/configs/40-libinput.conf /usr/share/X11/xorg.conf.d/40-libinput.conf
 sudo mkdir ~/.config/polybar
 sudo mv ~/.local/share/zaidan/configs/polybar-launch.sh ~/.config/polybar/launch_polybar.sh
+sudo chmod +x ~/.config/polybar/launch_polybar.sh
 sudo mv ~/.local/share/zaidan/configs/i3 ~/.config/i3/config
 sudo apt install -y polybar
