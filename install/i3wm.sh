92c1e8af97789470208ccf65fc3a93a9d44984b8
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-19 23:34:42 +0000

    Setting desktop background

--- install/i3wm.sh
+++ install/i3wm.sh
@@ -1,8 +1,19 @@
-sudo apt install -y i3
+# Install desktop UI packages
+sudo apt install -y i3 feh
+
+# Copy across config for keyboard and mouse things
 sudo mv ~/.local/share/zaidan/configs/X11/40-libinput.conf /usr/share/X11/xorg.conf.d/40-libinput.conf
+
+# Copy across Polybar config
 sudo mkdir -p ~/.config/polybar
 sudo mv ~/.local/share/zaidan/configs/polybar/launch.sh ~/.config/polybar/launch.sh
 sudo chmod +x ~/.config/polybar/launch.sh
 sudo mv ~/.local/share/zaidan/configs/polybar/config.ini ~/.config/polybar/config.ini
+
+# Copy across i3 config
 sudo mv ~/.local/share/zaidan/configs/i3/config ~/.config/i3/config
+
+# Move desktop background image
+suod mv ~/.local/share/zaidan/assets/background.png ~/background.png
+
 sudo apt install -y polybar
