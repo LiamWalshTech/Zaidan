318170a8bd19027965425cec9a610c3283bc1486
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-19 22:39:10 +0000

    Shifting renamed

--- install/i3wm.sh
+++ install/i3wm.sh
@@ -1,8 +1,8 @@
 sudo apt install -y i3
-sudo mv ~/.local/share/zaidan/configs/40-libinput.conf /usr/share/X11/xorg.conf.d/40-libinput.conf
+sudo mv ~/.local/share/zaidan/configs/X11/40-libinput.conf /usr/share/X11/xorg.conf.d/40-libinput.conf
 sudo mkdir -p ~/.config/polybar
-sudo mv ~/.local/share/zaidan/configs/polybar-launch.sh ~/.config/polybar/launch_polybar.sh
-sudo chmod +x ~/.config/polybar/launch_polybar.sh
-sudo mv ~/.local/share/zaidan/configs/polybar-config.ini ~/.config/polybar/config.ini
-sudo mv ~/.local/share/zaidan/configs/i3 ~/.config/i3/config
+sudo mv ~/.local/share/zaidan/configs/polybar/launch.sh ~/.config/polybar/launch.sh
+sudo chmod +x ~/.config/polybar/launch.sh
+sudo mv ~/.local/share/zaidan/configs/polybar/config.ini ~/.config/polybar/config.ini
+sudo mv ~/.local/share/zaidan/configs/i3/config ~/.config/i3/config
 sudo apt install -y polybar
