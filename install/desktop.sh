b652a90b70d8c775c20e93ec1f2d65281d425cc1
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-21 23:40:06 +0000

    Adding desktop GUI configs

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,2 +1,8 @@
+# Other stuff
+sudo mv ~/.local/share/zaidan/configs/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+sudo mv ~/.local/share/zaidan/configs/gtk-4.0/settings.ini ~/.config/gtk-4.0/settings.ini
+sudo mv ~/.local/share/zaidan/configs/xdg-desktop-portal/portals.conf ~/.config/xdg-desktop-portal/portals.conf
+
+# Gnome settings
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
