29eafedb0152fb288a263767df32405158157e13
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-03-21 21:44:16 +0000

    Sudo it

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -1,3 +1,3 @@
 sudo apt install -y alacritty
-curl -sS https://raw.githubusercontent.com/EdenEast/nightfox.nvim/7557f26defd093c4e9bc17f28b08403f706f5a44/extra/nordfox/alacritty.toml >> ~/.local/share/zaidan/configs/alacritty/alacritty.toml
+sudo curl -sS https://raw.githubusercontent.com/EdenEast/nightfox.nvim/7557f26defd093c4e9bc17f28b08403f706f5a44/extra/nordfox/alacritty.toml >> ~/.local/share/zaidan/configs/alacritty/alacritty.toml
 sudo mv ~/.local/share/zaidan/configs/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml 
