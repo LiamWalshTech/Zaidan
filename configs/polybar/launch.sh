ee5beb43ffc7c2e26f83ae5e242d00a58ce5f346
Liam Walsh <123986569+LiamWalshTech@users.noreply.github.com>
2025-04-02 20:50:48 +0000

    Fixing for just main screen

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -1,1 +1,7 @@
-polybar --reload main &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected primary" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
