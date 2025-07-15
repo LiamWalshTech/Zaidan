78a285d933b0bdb8c53a194b5d85463ee85f9c1e
LiamWalshTech <admin@liamwalsh.tech>
2025-07-15 21:37:25 +0100

    Creating tmux config directory before moving file

--- install/tmux.sh
+++ install/tmux.sh
@@ -2,4 +2,5 @@ echo "Installing tmux..."
 sudo apt install -y tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+sudo mkdir -p ~/.config/tmux
 sudo mv -f ~/.local/share/zaidan/configs/tmux/tmux.conf ~/.config/tmux/tmux.conf
